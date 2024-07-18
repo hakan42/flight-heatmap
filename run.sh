6e87ee21f0c4fbeace74e413fbbea9e4e32bc561
Hakan Tandogan <hakan@tandogan.com>
2024-07-18 22:01:05 +0200

    A runner to loop over the years
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

---
+++ run.sh
@@ -0,0 +1,63 @@
+#!/bin/sh -x
+
+export DOCKER_IMAGE_NAME=gshome/flight-heatmap
+export DOCKER_IMAGE=${DOCKER_IMAGE_NAME}:latest
+
+export NAME=heatmap-$(id -un)
+
+
+SHELL_MODE="-it"
+ARGUMENTS=""
+YEARS=""
+
+for arg in $@
+do
+    case "${arg}" in
+        -s|--shell)
+            SHELL_MODE="-it --entrypoint /bin/bash"
+            ;;
+
+        -d|--docker)
+            SHELL_MODE=""
+            ;;
+
+        *)
+            YEARS="${YEARS} ${arg}"
+            ;;
+    esac
+done
+
+if [ -z "${YEARS}" ]
+then
+    YEARS=2024
+fi
+     
+
+if [ -z ${WORKSPACE} ]
+then
+    HERE=$(dirname $0)
+else
+    HERE=${WORKSPACE}
+fi
+
+DATA=${HERE}/data
+DATA=$(realpath ${DATA})
+
+for YEAR in ${YEARS}
+do
+    rm -rf ${DATA}/output-${YEAR}
+    mkdir -p ${DATA}/output-${YEAR}
+
+    docker run \
+           --rm \
+           ${SHELL_MODE} \
+           --name ${NAME} \
+           --hostname ${NAME} \
+           --env USER_ID=$(id -u) \
+           --volume /etc/localtime:/etc/localtime:ro \
+           --volume /etc/timezone:/etc/timezone:ro \
+           --volume /etc/resolv.conf:/etc/resolv.conf:ro \
+           --mount type=bind,source=${DATA}/${YEAR},target=/app/gpx_files \
+           --mount type=bind,source=${DATA}/output-${YEAR},target=/app/output \
+           ${DOCKER_IMAGE}
+done
