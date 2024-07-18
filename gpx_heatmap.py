a36d02b328b61e7c0723b903830f25af4be0a19d
Hakan Tandogan <hakan@tandogan.com>
2024-07-18 22:46:59 +0200

    Draw a thin blue line with the real track
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- gpx_heatmap.py
+++ gpx_heatmap.py
@@ -5,6 +5,7 @@ import folium
 import os
 from folium.plugins import HeatMap
 
+
 def read_gpx_files(directory):
     data = []
     for file in os.listdir(directory):
@@ -13,13 +14,22 @@ def read_gpx_files(directory):
                 gpx = gpxpy.parse(gpx_file)
                 for track in gpx.tracks:
                     for segment in track.segments:
+                        segment_points = []
                         for point in segment.points:
                             data.append([point.latitude, point.longitude, point.elevation, point.time])
-    return pd.DataFrame(data, columns=['lat', 'lon', 'elevation', 'time'])
+                            segment_points.append([point.latitude, point.longitude])
+                        if segment_points:
+                            yield segment_points  # Yield each segment's points to be plotted as lines
 
-def generate_heatmap(data, output_file='heatmap.html'):
+def generate_heatmap(data, line_segments, output_file='heatmap.html'):
     map_center = [data['lat'].mean(), data['lon'].mean()]
     folium_map = folium.Map(location=map_center, zoom_start=12)
+
+    # Add tracks as thin lines
+    for segment in line_segments:
+        folium.PolyLine(segment, color="blue", weight=1.5, opacity=0.7).add_to(folium_map)
+
+    # Add heatmap layer
     heat_data = [[row['lat'], row['lon']] for index, row in data.iterrows()]
     HeatMap(heat_data).add_to(folium_map)
     folium_map.save(output_file)
@@ -43,5 +53,6 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    data = read_gpx_files(args.directory)
-    generate_heatmap(data, args.output_html)
+    line_segments = list(read_gpx_files(args.directory))
+    data = pd.DataFrame([point for segment in line_segments for point in segment], columns=['lat', 'lon'])
+    generate_heatmap(data, line_segments, args.output_html)
