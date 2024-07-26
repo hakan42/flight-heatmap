fab7ff1d9116c8c8be266a147cb6a9443ceb8507
Hakan Tandogan <hakan@tandogan.com>
2024-07-26 12:42:02 +0200

    A script to orchestrate periodic cron run
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

---
+++ cronner.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z ${WORKSPACE} ]
+then
+    HERE=$(dirname $0)
+else
+    HERE=${WORKSPACE}
+fi
+
+HERE=$(realpath ${HERE})
+LOG=${HERE}/data/log
+
+rm -rf ${HERE}/data
+mkdir -p ${LOG}
+
+export NAME=heatmap-$(id -un)
+docker rm -f ${NAME}        > ${LOG}/docker-rm.out     2> ${LOG}/docker-rm.err
+
+sh ${HERE}/fetch-data.sh    > ${LOG}/fetch-data.out    2> ${LOG}/fetch-data.err
+sh ${HERE}/run.sh --all     > ${LOG}/run.out           2> ${LOG}/run.err
+sh ${HERE}/push-heatmaps.sh > ${LOG}/push-heatmaps.out 2> ${LOG}/push-heatmaps.err
