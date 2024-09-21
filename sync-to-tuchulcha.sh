74ab4c38a692f04a625f58cd4f1dd1a8665bce07
Hakan Tandogan <hakan@tandogan.com>
2024-09-21 10:17:43 +0200

    Sync setup between seperate servers
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

---
+++ sync-to-tuchulcha.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -x
+
+rsync -avr ${HOME}/src/aviation/flight-heatmap/ tuchulcha:${HOME}/src/aviation/flight-heatmap/
---
+++ sync-to-wallaby.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -x
+
+rsync -avr tuchulcha:${HOME}/src/aviation/flight-heatmap/ ${HOME}/src/aviation/flight-heatmap/
